d8658de24caddd0348fa220d8ebcc4a5509cfc20
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2023-02-21 17:02:55 +0100

    pageCOntroller state items selected va riguardato

--- lib/features/categories/presentation/pages/category_screen.dart
+++ lib/features/categories/presentation/pages/category_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:multi_store/features/categories/presentation/cubit/categories_cubit.dart';
-import 'package:multi_store/features/categories/presentation/widgets/categories_view.dart';
-import 'package:multi_store/features/categories/presentation/widgets/side_navigator.dart';
-import 'package:multi_store/features/search/presentation/widgets/fake_search_app_bar.dart';
+import '../cubit/categories_cubit.dart';
+
+import '../../../search/presentation/widgets/fake_search_app_bar.dart';
 
 import '../../domain/entities/category_entity.dart';
 
@@ -15,7 +14,13 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-  late final List<Category> items;
+  PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
@@ -25,34 +30,89 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const FakeSearchAppBar(),
-      ),
-      body: Stack(
-        children: [
-          BlocConsumer<CategoriesCubit, CategoriesState>(
-            listener: (context, state) {
-              // TODO: implement listener
-            },
-            builder: (context, state) {
-              return Positioned(
-                  bottom: 0,
-                  left: 0,
-                  child: SideNavigator(
-                    items: state.mainCategories,
-                    size: MediaQuery.of(context).size,
-                  ));
-            },
+    return BlocBuilder<CategoriesCubit, CategoriesState>(
+      buildWhen: (previous, current) => previous != current,
+      builder: (context, state) {
+        return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              title: const FakeSearchAppBar(),
+            ),
+            body: Stack(
+              children: [
+                Positioned(
+                    bottom: 0,
+                    left: 0,
+                    child: sideNavigator(
+                        items: state.mainCategories,
+                        size: MediaQuery.of(context).size,
+                        selectedIndex: state.mainCategories
+                            .indexWhere((element) => element.isSelected))),
+                Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: categoriesView(
+                      size: MediaQuery.of(context).size,
+                    )),
+              ],
+            ));
+      },
+    );
+  }
+
+  Widget sideNavigator(
+      {required Size size,
+      required List<Category> items,
+      required int selectedIndex}) {
+    return SizedBox(
+      height: size.height * 0.8,
+      width: size.width * 0.2,
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () {
+            context.read<CategoriesCubit>().getMainCategories(index);
+            _pageController.jumpToPage(selectedIndex);
+          },
+          child: Container(
+            color:
+                items[index].isSelected ? Colors.white : Colors.grey.shade300,
+            height: 100,
+            child: Center(
+                child: Text(
+              items[index].label,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 12),
+            )),
           ),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              child: CategoriesView(
-                size: MediaQuery.of(context).size,
-              )),
+        ),
+      ),
+    );
+  }
+
+  Widget categoriesView({required Size size}) {
+    return Container(
+      color: Colors.white,
+      height: size.height * 0.8,
+      width: size.width * 0.8,
+      child: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          context.read<CategoriesCubit>().getMainCategories(index);
+        },
+        scrollDirection: Axis.vertical,
+        children: const [
+          Center(child: Text('men ctegory')),
+          Center(child: Text('women ctegory')),
+          Center(child: Text('electronics ctegory')),
+          Center(child: Text('accesories ctegory')),
+          Center(child: Text('shoes ctegory')),
+          Center(child: Text('home & garden ctegory')),
+          Center(child: Text('beauty ctegory')),
+          Center(child: Text('kids ctegory')),
+          Center(child: Text('bags ctegory')),
         ],
       ),
     );
